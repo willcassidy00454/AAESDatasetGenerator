a2e85bd4f1dd374ef0e15fcdeb284c7a3e3aeffc
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2025-03-06 17:12:58 +0000

    Add permutation, routing, RT ratio and filter indexing to the AAES simulation

--- Simulation Src/SimulateAAESDataset.m
+++ Simulation Src/SimulateAAESDataset.m
@@ -2,6 +2,8 @@
 rir_base_dir = "Audio Data/Physical RIRs/";
 reverberator_base_dir = "Audio Data/Reverberators/";
 output_base_dir = "Audio Data/AAES Receiver RIRs/";
+permutations_base_dir = "Simulation Parameters/Permutations/";
+routings_base_dir = "Simulation Parameters/Routings/";
 
 loop_gains = readmatrix("Simulation Parameters/Loop Gains/LoopGains.dat");
 
@@ -14,24 +16,32 @@ num_ls = 16;
 num_loop_gains = length(loop_gains);
 num_rooms = 3;
 num_absorptions = 3;
-num_reverberators = 4;
+num_rt_ratios = 3;
+num_filters = 3;
+num_routings = 4;
 
 % Make matrix of combinations where each row defines each simulation
 % Columns: room index, absorptions index, reverberator index, loop gain (dB
 % from GBI)
-conditions = zeros(num_loop_gains * num_rooms * num_absorptions * num_reverberators, 4);
+conditions = zeros(num_loop_gains * num_rooms * num_absorptions * num_rt_ratios * num_filters * num_routings, 6);
 
 row = 1;
 for loop_gain_index = 1:num_loop_gains
-    for reverberator_index = 1:num_reverberators
-        for absorptions_index = 1:num_absorptions
+    for rt_ratio_index = 1:num_rt_ratios
+        for absorption_index = 1:num_absorptions
             for room_index = 1:num_rooms
-                conditions(row, 1) = room_index;
-                conditions(row, 2) = absorptions_index;
-                conditions(row, 3) = reverberator_index;
-                conditions(row, 4) = loop_gains(loop_gain_index);
-
-                row = row + 1;
+                for routing_index = 1:num_routings
+                    for filter_index = 1:num_filters
+                        conditions(row, 1) = room_index;
+                        conditions(row, 2) = absorption_index;
+                        conditions(row, 3) = rt_ratio_index;
+                        conditions(row, 4) = filter_index;
+                        conditions(row, 5) = loop_gains(loop_gain_index);
+                        conditions(row, 6) = routing_index;
+        
+                        row = row + 1;
+                    end
+                end
             end
         end
     end
@@ -40,17 +50,26 @@ end
 % Simulate each row in the conditions matrix
 for row = 1:size(conditions, 1)
     room_index = conditions(row, 1);
-    absorptions_index = conditions(row, 2);
-    reverberator_index = conditions(row, 3);
-    loop_gain = conditions(row, 4);
+    absorption_index = conditions(row, 2);
+    rt_ratio_index = conditions(row, 3);
+    filter_index = conditions(row, 4);
+    loop_gain = conditions(row, 5);
+    routing_index = conditions(row, 6);
 
-    GenerateAAESIRs(rir_base_dir + "Room "+room_index+" Absorption "+absorptions_index+"/", ...
-        reverberator_base_dir + "Reverberator "+reverberator_index+"/", ...
-        output_base_dir + "AAES Room "+room_index+" Abs "+absorptions_index+" Rev "+reverberator_index+" LG "+loop_gain+"/", ...
+    % Load routing and multiply with permutation corresponding to this room
+    raw_routing = readmatrix(routings_base_dir + "routing_" + routing_index + ".dat");
+    permutation = readmatrix(permutations_base_dir + "room_" + room_index + ".dat");
+    routing = raw_routing * permutation;
+
+    GenerateAAESIRs(rir_base_dir + "Room "+room_index+" Absorption "+absorption_index+"/", ...
+        reverberator_base_dir + "Reverberator Room "+room_index+" Absorption "+absorption_index+" RT "+rt_ratio_index+" Filter "+filter_index+"/", ...
+        output_base_dir + "AAES Room "+room_index+" Absorption "+absorption_index+" Reverberator "+rt_ratio_index+" Loop Gain "+loop_gain+"/", ...
         loop_gain, ...
         num_ls, ...
         num_mics, ...
         bit_depth, ...
         true, ...
-        true);
+        true, ...
+        true, ...
+        routing);
 end
