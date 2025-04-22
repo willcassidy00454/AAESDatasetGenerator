da3ee2b92499f8a3298b3d6f869262161e244963
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2025-04-22 09:52:46 +0100

    Refactoring and cleanup CreateMaterials

--- Treble/CreateMaterials.py
+++ Treble/CreateMaterials.py
@@ -11,28 +11,29 @@ tsdk = TSDK()
 f_axis_oct = np.asarray([63, 125, 250, 500, 1000, 2000, 4000, 8000])
 
 # Absorption coefficients (surface_index (floor, ceiling, front wall, other walls), octave_band)
-absorption_coefficients = np.asarray([[	0.2288,	0.1352,	0.16,	0.1659,	0.1892,	0.198,	0.2247,	0.1819	],
+absorption_coefficients = np.asarray([[	0.39552,	0.7004,	2.168,	2.1516,	2.148,	1.8036,	1.573,	0.715	],
 
-[	0.2948,	0.1742,	0.22,	0.1974,	0.1694,	0.086,	0.0903,	0.0731	],
+[	0.1728,	0.306,	0.48,	0.33,	0.27,	0.216,	0.1925,	0.0875	],
 
-[	0.1936,	0.1144,	0.085,	0.1323,	0.1254,	0.156,	0.2016,	0.1632	],
+[	0.270048,	0.47821,	1.822,	1.31505,	1.2156,	1.26576,	0.69025,	0.31375	],
 
-[	0.352,	0.208,	0.15,	0.2394,	0.2244,	0.28,	0.3612,	0.2924	]
-									])
+[	0.0192,	0.034,	0.08,	0.099,	0.09,	0.108,	0.1375,	0.0625	]
+
+								])
 
 scattering_coefficients = [0.5, 0.15, 0.4, 0.3]
 
 # Clamp coefficients
-absorption_coefficients = absorption_coefficients.clip(0.0, 0.95)
+absorption_coefficients = absorption_coefficients.clip(0.01, 0.95)
 
 #%% Create and fit all surface materials
-room_num_label = 1
+room_index = 2 # Starts at 0
 
 fitted_materials = []
-material_names = [f"room_{room_num_label}_floor",
-                  f"room_{room_num_label}_ceiling",
-                  f"room_{room_num_label}_front_wall",
-                  f"room_{room_num_label}_non_frontal_wall"]
+material_names = [f"room_{room_index + 1}_floor",
+                  f"room_{room_index + 1}_ceiling",
+                  f"room_{room_index + 1}_front_wall",
+                  f"room_{room_index + 1}_non_frontal_wall"]
 
 for mat_index in range(4):
     # Create the material definition object
@@ -49,13 +50,13 @@ for mat_index in range(4):
     fitted_materials.append(tsdk.material_library.perform_material_fitting(material_definition))
 
 #%% Visualise the fitting results
-mat_index_to_plot = 0
+mat_index_to_plot = 3
 
 # Retrieve the absorption coefficients from the fitted material
-fitted_material_abs = fitted_materials[mat_index_to_plot].absorption_coefficients
+fitted_material_coeffs = fitted_materials[mat_index_to_plot].absorption_coefficients
 
 plt.semilogx(f_axis_oct, absorption_coefficients[mat_index_to_plot], label='Original Coefficients')
-plt.semilogx(f_axis_oct, fitted_material_abs, label='Fitted Coefficients')
+plt.semilogx(f_axis_oct, fitted_material_coeffs, label='Fitted Coefficients')
 plt.grid(which='both')
 plt.xlim([50, 10000])
 plt.ylim([0,1])
@@ -65,12 +66,10 @@ plt.title('Input vs. Fitted Absorption Coefficients')
 plt.legend()
 plt.show()
 
-#%% Delete existing materials
+#%% Replace materials
 for mat_index in range(4):
     material = tsdk.material_library.search(material_names[mat_index])[0]
     tsdk.material_library.delete(material)
 
-#%% Save materials
-for mat_index in range(4):
     current_material = tsdk.material_library.create(fitted_materials[mat_index])
     dd.as_tree(current_material)
