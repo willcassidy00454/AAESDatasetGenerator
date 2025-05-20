4eddd4a363bd0d95256d1c144d2b3a7639f108da
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2025-05-20 11:57:58 +0100

    Add dataset subset selection during generation

--- Simulation Src/SimulateAAESDataset.m
+++ Simulation Src/SimulateAAESDataset.m
@@ -13,17 +13,22 @@ num_mics = 16;
 num_ls = 16;
 
 % Parameters to combine
-num_loop_gains = length(loop_gains);
 num_rooms = 3;
 num_absorptions = 3;
 num_rt_ratios = 3;
 num_filters = 3;
+num_loop_gains = length(loop_gains);
 num_routings = 4;
+
+num_conditions_to_select = 112; % Selects a subset of the full dataset
+random_condition_offset = 3; % Randomly selects neighbours of the uniformly
+% sampled conditions by up to this value. Use this to avoid missing an
+% argument entirely due to the regularity of uniform sampling.
 
 % Make matrix of combinations where each row defines each simulation
 % Columns: room index, absorptions index, reverberator index, loop gain (dB
 % from GBI)
-conditions = zeros(num_loop_gains * num_rooms * num_absorptions * num_rt_ratios * num_filters * num_routings, 6);
+all_conditions = zeros(num_loop_gains * num_rooms * num_absorptions * num_rt_ratios * num_filters * num_routings, 6);
 
 row = 1;
 for loop_gain_index = 1:num_loop_gains
@@ -32,12 +37,12 @@ for loop_gain_index = 1:num_loop_gains
             for room_index = 1:num_rooms
                 for routing_index = 1:num_routings
                     for filter_index = 1:num_filters
-                        conditions(row, 1) = room_index;
-                        conditions(row, 2) = absorption_index;
-                        conditions(row, 3) = rt_ratio_index;
-                        conditions(row, 4) = filter_index;
-                        conditions(row, 5) = loop_gain_index;
-                        conditions(row, 6) = routing_index;
+                        all_conditions(row, 1) = room_index;
+                        all_conditions(row, 2) = absorption_index;
+                        all_conditions(row, 3) = rt_ratio_index;
+                        all_conditions(row, 4) = filter_index;
+                        all_conditions(row, 5) = loop_gain_index;
+                        all_conditions(row, 6) = routing_index;
         
                         row = row + 1;
                     end
@@ -47,14 +52,23 @@ for loop_gain_index = 1:num_loop_gains
     end
 end
 
+reduced_conditions = zeros(num_conditions_to_select, size(all_conditions, 2));
+step = floor(size(all_conditions, 1) / num_conditions_to_select);
+
+for condition_index = 1:num_conditions_to_select
+    stepped_condition_index = (condition_index * step) + randi(random_condition_offset);
+    % % % add catch for when the rand pushed out of bounds
+    reduced_conditions(condition_index,:) = all_conditions(stepped_condition_index, :);
+end
+
 % Simulate each row in the conditions matrix
-parfor row = 1:size(conditions, 1)
-    room_index = conditions(row, 1);
-    absorption_index = conditions(row, 2);
-    rt_ratio_index = conditions(row, 3);
-    filter_index = conditions(row, 4);
-    loop_gain_index = conditions(row, 5);
-    routing_index = conditions(row, 6);
+parfor row = 1:size(reduced_conditions, 1)
+    room_index = reduced_conditions(row, 1);
+    absorption_index = reduced_conditions(row, 2);
+    rt_ratio_index = reduced_conditions(row, 3);
+    filter_index = reduced_conditions(row, 4);
+    loop_gain_index = reduced_conditions(row, 5);
+    routing_index = reduced_conditions(row, 6);
 
     if routing_index == 4 % For routing 4, randomly fill one row (all mics to one LS) 
         routing = zeros(16);
